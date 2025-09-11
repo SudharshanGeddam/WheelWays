9763283fd6a625e082efb80af838786b0147ccff
Sudharshan Geddam <darshans1013@gmail.com>
2025-09-11 13:30:18 +0530

    Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wheelways/Screens/welcome_screen.dart';
+import 'package:wheelways/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
