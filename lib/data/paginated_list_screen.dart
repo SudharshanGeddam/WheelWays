1296ab6e7d2327d8c3b7713284450333c3236464
Sudharshan Geddam <darshans1013@gmail.com>
2025-09-14 17:58:45 +0530

    Return & Request

---
+++ lib/data/paginated_list_screen.dart
@@ -0,0 +1,158 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:wheelways/pages/request_page.dart';
+
+class PaginatedListScreen extends StatefulWidget {
+  const PaginatedListScreen({super.key});
+
+  @override
+  State<PaginatedListScreen> createState() => _PaginatedListScreenState();
+}
+
+class _PaginatedListScreenState extends State<PaginatedListScreen> {
+  final ScrollController _scrollController = ScrollController();
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+  final int _limit = 10;
+
+  List<DocumentSnapshot> bikes = [];
+  DocumentSnapshot? lastDoc;
+  bool isLoading = false;
+  bool hasMore = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchBikes();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+              _scrollController.position.maxScrollExtent &&
+          !isLoading &&
+          hasMore) {
+        _fetchBikes();
+      }
+    });
+  }
+
+  Future<void> _fetchBikes() async {
+    if (isLoading) return;
+
+    setState(() => isLoading = true);
+
+    try {
+      Query query = db
+          .collection('BikesData')
+          .orderBy('createdAt', descending: true)
+          .where('isAllocated', isEqualTo: false)
+          .where('isDamaged', isEqualTo: false)
+          .limit(_limit);
+
+      if (lastDoc != null) {
+        query = query.startAfterDocument(lastDoc!);
+      }
+
+      QuerySnapshot querySnapshot = await query.get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        lastDoc = querySnapshot.docs.last;
+        setState(() {
+          bikes.addAll(querySnapshot.docs);
+        });
+
+        if (querySnapshot.docs.length < _limit) {
+          hasMore = false;
+        }
+      } else {
+        hasMore = false;
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: bikes.length + 1,
+      itemBuilder: (context, index) {
+        if (index < bikes.length) {
+          var data = bikes[index].data() as Map<String, dynamic>;
+          return Card(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/bike_img.jpg',
+                        height: 40,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(width: 10),
+                      const Text('HCL Bikes'),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Text('Color: ${data['bikeColor']}'),
+                  const SizedBox(height: 5),
+                  Text('Bike Id: ${data['bikeId']}'),
+                  const SizedBox(height: 5),
+                  Text('Location: ${data['bikeLocation']}'),
+                  const SizedBox(height: 5),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        try {
+                          await db
+                              .collection('BikesData')
+                              .doc(bikes[index].id)
+                              .update({'isAllocated': true});
+
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RequestPage(
+                                bikeId : data['bikeId'],
+                                bikeColor : data['bikeColor'],
+                                bikeLocation : data['bikeLocation'],
+                              ),
+                            ),
+                          );
+                        } catch (e) {
+                          print('Update failed');
+                        }
+                      },
+                      child: Text('Request'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return hasMore
+              ? Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Lottie.asset(
+                      'assets/lotties/Loading.json',
+                      width: 250,
+                      height: 250,
+                    ),
+                  ),
+                )
+              : SizedBox();
+        }
+      },
+    );
+  }
+}
---
+++ lib/pages/request_page.dart
@@ -0,0 +1,110 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class RequestPage extends StatefulWidget {
+  final String bikeId;
+  final String bikeColor;
+  final String bikeLocation;
+
+  const RequestPage({
+    super.key,
+    required this.bikeId,
+    required this.bikeColor,
+    required this.bikeLocation,
+  });
+
+  @override
+  State<RequestPage> createState() => _RequestPageState();
+}
+
+class _RequestPageState extends State<RequestPage> {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  bool _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Wheel Ways'), centerTitle: true),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text('BiCycle Allocated'),
+              const SizedBox(height: 10),
+              Lottie.asset('assets/lotties/Bicycle_return.json'),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/bike_img.jpg',
+                            height: 40,
+                            fit: BoxFit.cover,
+                          ),
+                          const SizedBox(width: 10),
+                          const Text('HCL Bikes'),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                      Text('Color: ${widget.bikeColor}'),
+                      const SizedBox(height: 5),
+                      Text('Bike Id: ${widget.bikeId}'),
+                      const SizedBox(height: 5),
+                      Text('Location: ${widget.bikeLocation}'),
+                      const SizedBox(height: 5),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: ElevatedButton(
+                          onPressed: _isLoading
+                              ? null
+                              : () async {
+                                  setState(() => _isLoading = true);
+                                  try {
+                                    await db
+                                        .collection('BikesData')
+                                        .doc(widget.bikeId)
+                                        .update({'isAllocated': false});
+
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text('Return Successful'),
+                                      ),
+                                    );
+
+                                    Navigator.pop(context);
+                                  } catch (e) {
+                                    print(e);
+                                  } finally {
+                                    if (mounted) {
+                                      setState(() => _isLoading = false);
+                                    }
+                                  }
+                                },
+                          child: _isLoading
+                              ? SizedBox(
+                                  height: 100,
+                                  width: 100,
+                                  child: Lottie.asset(
+                                    'assets/lotties/Loading.json',
+                                  ),
+                                )
+                              : const Text('Return'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
